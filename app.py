bfa5094852e385503717ebfafe28eb00d02cc58a
kdjordan <k.dean.jordan@gmail.com>
2022-04-20 19:44:39 -0700

    need: timer fucntionality and tests

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from boggle import Boggle
-from flask import Flask, request, render_template, redirect, flash, session
+from flask import Flask, request, render_template, redirect, flash, session, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
@@ -10,15 +10,48 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 boggle_game = Boggle()
 
+
+def check_word(wrd):
+    res = boggle_game.check_valid_word(session['BOARD'], wrd)
+    if res == 'ok':
+        the_list = session['GOOD_WORDS']
+        the_list.append(wrd)
+        session['GOOD_WORDS'] = the_list
+    elif res == 'not-on-board':
+        the_list = session['NOT_ON_BOARD']
+        the_list.append(wrd)
+        session['NOT_ON_BOARD'] = the_list
+    elif res == 'not-word':
+        the_list = session['NOT_WORD']
+        the_list.append(wrd)
+        session['NOT_WORD'] = the_list
+    return res
+
+
 @app.route('/')
 def index():
-    """render title and instructions for our imported survey"""
-    return render_template('base.html')
+    """Set up session elements and redirect to make board with info"""
+    session['BOARD'] = boggle_game.make_board()
+    session['GOOD_WORDS'] = []
+    session['NOT_ON_BOARD'] = []
+    session['NOT_WORD'] = []
+    return redirect('/board')
+
 
 @app.route('/board')
 def board():
-    the_board = boggle_game.make_board()
-    return render_template('board.html', board=the_board)
+    """sets up game and renders template to begin game"""
+    return render_template('board.html')
+
+
+@app.route('/submit', methods=["POST"])
+def submit():
+    """Checks for word in board from submitted ajax call, returns JSON of res and the word"""
+    word = request.get_json()['word']
+    # check word in board and update session
+    res = check_word(word)
+    return jsonify({res: word})
+
 
 
 
--- static/app.js
+++ static/app.js
@@ -2,10 +2,16 @@ let startBtn = document.getElementById('start-btn')
 let counter = document.getElementById('counter')
 let goBtn = document.getElementById('go-btn')
 let wordInput = document.getElementById('word-input')
-let  theForm = document.getElementById('the-form')
+let theForm = document.getElementById('the-form')
+let goodList = document.getElementById('good-list')
+let badList = document.getElementById('bad-list')
+let goodCount = document.getElementById('good-word-count')
+let badCount = document.getElementById('bad-word-count')
+let goodCountNum = 0
+let badCountNum = 0
 
 let countdownInterval
-
+console.log(goodCount.innerText)
 //event handlers
 startBtn.addEventListener('click', ()=> {
     let count = 5
@@ -22,9 +28,31 @@ startBtn.addEventListener('click', ()=> {
     }, 1000)
 })
 
-goBtn.addEventListener('click', (e)=> {
+goBtn.addEventListener('click', async (e)=> {
     e.preventDefault()
-    console.log(wordInput.value)
-    theForm.reset()
+    try {
+        let word = wordInput.value
+        theForm.reset()
+        let res = await axios.post('http://127.0.0.1:5000/submit', {'word': word})
 
+        if (Object.keys(res.data)[0] == 'ok') {
+            console.log('append to good', Object.values(res.data)[0])
+            addToList(goodList, Object.values(res.data)[0])
+            goodCount.innerText = goodCountNum += 1
+        } else {
+            console.log('append to bad', Object.values(res.data)[0])
+            addToList(badList, Object.values(res.data)[0])
+            badCount.innerText = badCountNum += 1
+            console.log('bad count', badCountNum)
+        }
+    } catch(e) {
+        console.log('error in axios', e)
+    }
 })
+
+//UI function to add to word lists
+function addToList(list, word) {
+    var li = document.createElement("li")
+    li.innerText = word
+    list.appendChild(li)
+}
